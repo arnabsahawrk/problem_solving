7c71a808327139de23cf1541e7840e696eda28e3
arnabw <arnabsahawrk@gmail.com>
2024-11-02 18:30:21 +0600

    11/2/24

---
+++ CodeChef/C/rightThere.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int n, x;
+        scanf("%d %d", &n, &x);
+
+        if (n > x)
+            printf("NO\n");
+        else
+            printf("YES\n");
+    }
+
+    return 0;
+}
