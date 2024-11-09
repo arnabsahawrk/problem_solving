620adf9edc24cf4112166fefc0db45ab408ab1d7
arnabw <arnabsahawrk@gmail.com>
2024-11-09 23:26:02 +0600

    11/9/24

---
+++ HackerRank/C/bitwiseOperators.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    int n, k;
+    int c = 0;
+    int maxAnd = 0, maxOr = 0, maxXor = 0;
+    scanf("%d %d", &n, &k);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i + 1; j <= n; j++)
+        {
+            c = i & j;
+            if (c > maxAnd && c < k)
+                maxAnd = c;
+        }
+    }
+    printf("%d\n", maxAnd);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i + 1; j <= n; j++)
+        {
+            c = i | j;
+            if (c > maxOr && c < k)
+                maxOr = c;
+        }
+    }
+    printf("%d\n", maxOr);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i + 1; j <= n; j++)
+        {
+            c = i ^ j;
+            if (c > maxXor && c < k)
+                maxXor = c;
+        }
+    }
+    printf("%d\n", maxXor);
+
+    return 0;
+}
