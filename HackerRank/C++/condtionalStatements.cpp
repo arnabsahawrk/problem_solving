c5d34ae8e23172de3fa6471bc9aa4fb3cfbed1d9
arnabw <arnabsahawrk@gmail.com>
2024-11-16 09:08:54 +0600

    11/16/24

---
+++ HackerRank/C++/condtionalStatements.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int input;
+    cin >> input;
+
+    if (input == 1)
+        cout << "one";
+    else if (input == 2)
+        cout << "two";
+    else if (input == 3)
+        cout << "three";
+    else if (input == 4)
+        cout << "four";
+    else if (input == 5)
+        cout << "five";
+    else if (input == 6)
+        cout << "six";
+    else if (input == 7)
+        cout << "seven";
+    else if (input == 8)
+        cout << "eight";
+    else if (input == 9)
+        cout << "nine";
+    else
+        cout << "Greater than 9";
+
+    return 0;
+}
