b084d33290a098e067d22ee58b52f184eacde4c2
arnabw <arnabsahawrk@gmail.com>
2024-12-02 15:05:44 +0600

    12/2/24

---
+++ HackerRank/10 Days of Javascript/switch.js
@@ -0,0 +1,52 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function getLetter(s) {
+  let letter;
+  // Write your code here
+  switch (s[0]) {
+    case "a":
+      letter = "A";
+      break;
+    case "b":
+      letter = "B";
+      break;
+    case "h":
+      letter = "C";
+      break;
+    default:
+      letter = "D";
+  }
+
+  return letter;
+}
+
+function main() {
+  const s = readLine();
+
+  console.log(getLetter(s));
+}
