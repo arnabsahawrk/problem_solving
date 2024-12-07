b8563d458b83bf48bdf99ded7744d1399a35e0b6
arnabw <arnabsahawrk@gmail.com>
2024-12-07 16:59:19 +0600

    12/7/24

---
+++ CodeChef/500 difficulty rating/C++/chefAndWireFrames.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, m, x;
+        cin >> n >> m >> x;
+
+        cout << ((n * 2) + (m * 2)) * x << endl;
+    }
+}
