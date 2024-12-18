501fde3f56112f5141ff82598977c9311cb72092
arnabw <arnabsahawrk@gmail.com>
2024-12-18 14:45:58 +0600

    12/18/24

--- LeetCode/Miscellaneous/C++/206. Reverse Linked List.cpp
+++ LeetCode/Miscellaneous/C++/206. Reverse Linked List.cpp
@@ -15,7 +15,7 @@ class Solution
 public:
     ListNode *reverseList(ListNode *head)
     {
-        if (head == nullptr)
+        if (head == nullptr || head->next == nullptr)
             return head;
 
         ListNode *temp = head;
