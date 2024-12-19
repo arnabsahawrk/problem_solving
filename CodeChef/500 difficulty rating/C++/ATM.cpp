ad56dd777dc038d5845468d079177332e362a846
arnabw <arnabsahawrk@gmail.com>
2024-12-19 08:18:28 +0600

    12/19/24

---
+++ CodeChef/500 difficulty rating/C++/ATM.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    double y;
+    cin >> x >> y;
+
+    if (x % 5 == 0 && x + 0.50 <= y)
+    {
+        cout << fixed << setprecision(2) << y - (x + 0.50) << endl;
+    }
+    else
+        cout << fixed << setprecision(2) << y << endl;
+}
