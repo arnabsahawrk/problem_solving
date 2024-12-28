923b2a958b7a42bd01a77ad1a57ff21ac91abfd8
arnabw <arnabsahawrk@gmail.com>
2024-12-28 10:48:41 +0600

    12/28/24

---
+++ CodeChef/500 difficulty rating/C++/Maximum_Submissions.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x;
+        cin >> x;
+
+        x *= 60;
+
+        cout << x / 30 << endl;
+    }
+}
