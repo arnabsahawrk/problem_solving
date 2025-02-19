b9ac6bf88d0fed27055cdb81531b6745c1623657
arnabw <arnabsahawrk@gmail.com>
2025-02-19 22:00:30 +0600

    2/19/25

---
+++ CodeChef/Contest/START174/Too_Much_Homework.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+
+    if (x + (y * 10) >= 100)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
