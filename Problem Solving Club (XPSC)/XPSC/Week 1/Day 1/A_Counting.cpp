327fbc05d43a4dd93e0e1924135b70bdfff0248a
arnabw <arnabsahawrk@gmail.com>
2025-03-25 00:43:41 +0600

    3/24/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 1/Day 1/A_Counting.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b;
+    cin >> a >> b;
+
+    int count = 0;
+
+    for (int i = a; i <= b; i++)
+        count++;
+
+    cout << count;
+
+    return 0;
+}
