c9f89674a5567d1552faa88dcd74241571ca545a
arnabw <arnabsahawrk@gmail.com>
2025-04-22 19:44:47 +0600

    4/22/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 4/Day 4/A_Jellyfish_and_Undertale.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    ll a, b, n;
+    cin >> a >> b >> n;
+
+    ll count = b;
+    rep(i, 0, n)
+    {
+        ll x;
+        cin >> x;
+        count += min(x, a - 1);
+    }
+
+    cout << count << endl;
+}
+
+int main()
+{
+    fast_io;
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+        solve();
+
+    return 0;
+}
