0e576facc7c6058a888ae042d41f1ca8b75bc362
arnabw <arnabsahawrk@gmail.com>
2025-04-23 22:32:22 +0600

    4/23/25

---
+++ Problem Solving Club (XPSC)/XPSC/Contests/CodeChef/START183D/S_to_T.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    string s, t;
+    cin >> t >> s;
+
+    if (s == t)
+        cout << 0 << endl;
+    else if (s[0] != t[0])
+        cout << -1 << endl;
+    else
+    {
+        vi ans;
+
+        rrep(i, n - 1, 0)
+        {
+            if (s[i] != t[i])
+                ans.pb(i);
+        }
+
+        ll sz = ans.size();
+        cout << sz << endl;
+        each(x, ans) cout << x << " ";
+
+        cout << endl;
+    }
+}
+
+int main()
+{
+    fast_io;
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+        solve();
+
+    return 0;
+}
