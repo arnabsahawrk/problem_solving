ef7d1e5030a3853263e61562ae83377dac0e0456
arnabw <arnabsahawrk@gmail.com>
2025-04-23 13:41:15 +0600

    4/23/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 4/Day 5/A_Monsters_easy_version.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vi a(n);
+    rep(i, 0, n) cin >> a[i];
+
+    sort(all(a));
+
+    vi b(n);
+    b[0] = 1;
+    rep(i, 1, n) b[i] = min(b[i - 1] + 1, a[i]);
+
+    ll rslt = 0;
+    rep(i, 0, n) rslt += (a[i] - b[i]);
+
+    cout << rslt << endl;
+}
+
+int main()
+{
+    fast_io;
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+        solve();
+
+    return 0;
+}
---
+++ Problem Solving Club (XPSC)/XPSC/Week 4/Day 5/G_1_Teleporters_Easy_Version.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    int n, c;
+    cin >> n >> c;
+
+    vll arr(n);
+    rep(i, 0, n)
+    {
+        int x;
+        cin >> x;
+        x += i + 1;
+
+        arr[i] = x;
+    }
+
+    sort(all(arr));
+
+    int count = 0;
+    each(x, arr)
+    {
+        if (x <= c)
+        {
+            count++;
+            c -= x;
+        }
+    }
+
+    cout << count << endl;
+}
+
+int main()
+{
+    fast_io;
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+        solve();
+
+    return 0;
+}
