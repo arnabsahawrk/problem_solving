329cba10a02f76c5b8442dce6b3f1834f6b28190
arnabw <arnabsahawrk@gmail.com>
2025-04-25 02:39:18 +0600

    4/25/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 4/Day 6/A_Make_It_Zero.cpp
@@ -0,0 +1,61 @@
+/*   Author: Arnab Saha   Date: 04/25/2025 [01:47:53]   */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define fi first
+#define se second
+
+const int inf = 1e9;
+const ll linf = 1e18;
+const int mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    int n;
+    cin >> n;
+
+    vll arr(n);
+    rep(i, 0, n) cin >> arr[i];
+
+    if (n & 1)
+    {
+        cout << 4 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << n - 1 << " " << n << endl;
+        cout << n - 1 << " " << n << endl;
+    }
+    else
+    {
+        cout << 2 << endl;
+        cout << 1 << " " << n << endl;
+        cout << 1 << " " << n << endl;
+    }
+}
+
+int main()
+{
+    fast_IO;
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    return 0;
+}
