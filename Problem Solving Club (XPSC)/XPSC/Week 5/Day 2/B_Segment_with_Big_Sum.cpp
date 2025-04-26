597c0877ef429a83016b5636f96d9d9d2f7aa0a4
arnabw <arnabsahawrk@gmail.com>
2025-04-26 22:18:05 +0600

    4/26/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 5/Day 2/B_Segment_with_Big_Sum.cpp
@@ -0,0 +1,72 @@
+/*   Author: Arnab Saha  Date: 26/04/2025 [18:09:27]   */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define fi first
+#define se second
+
+const int inf = 1e9;
+const ll linf = 1e18;
+const int mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    ll n, k;
+    cin >> n >> k;
+
+    vll arr(n);
+    rep(i, 0, n) cin >> arr[i];
+
+    ll l = 0, r = 0, sum = 0, ans = LLONG_MAX;
+    while (r < n)
+    {
+        sum += arr[r];
+
+        if (sum >= k)
+        {
+            ans = min(ans, r - l + 1);
+
+            while (sum > k)
+            {
+                sum -= arr[l];
+                l++;
+
+                if (sum >= k)
+                    ans = min(ans, r - l + 1);
+            }
+        }
+
+        r++;
+    }
+    if (ans == LLONG_MAX)
+        cout << -1 << endl;
+    else
+        cout << ans << endl;
+}
+
+int main()
+{
+    fast_IO;
+
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    return 0;
+}
---
+++ Problem Solving Club (XPSC)/XPSC/Week 5/Day 2/E_Segments_with_Small_Set.cpp
@@ -0,0 +1,73 @@
+/*   Author: Arnab Saha  Date: 26/04/2025 [19:44:58]   */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define fi first
+#define se second
+
+const int inf = 1e9;
+const ll linf = 1e18;
+const int mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    ll n, k;
+    cin >> n >> k;
+
+    vll arr(n);
+    rep(i, 0, n) cin >> arr[i];
+
+    map<ll, ll> mp;
+    ll l = 0, r = 0, ans = 0;
+    while (r < n)
+    {
+        mp[arr[r]]++;
+
+        if (mp.size() <= k)
+            ans += (r - l + 1);
+        else
+        {
+            while (mp.size() > k)
+            {
+                mp[arr[l]]--;
+                if (mp[arr[l]] == 0)
+                    mp.erase(arr[l]);
+
+                l++;
+            }
+
+            ans += (r - l + 1);
+        }
+
+        r++;
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast_IO;
+
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    return 0;
+}
