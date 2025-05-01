4158d287b19f45eb63a0d29ada45c32dc767541b
arnabw <arnabsahawrk@gmail.com>
2025-05-01 16:27:31 +0600

    5/1/25 (26)

---
+++ Problem Solving Club (XPSC)/XPSC/Week 5/Day 6/C_Three_Parts_of_the_Array.cpp
@@ -0,0 +1,60 @@
+/*   Author: Arnab Saha  Date: 05/01/2025 [00:44:24]   */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define ld long double
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+const int inf = 1e9;
+const ll linf = 1e18;
+const int mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    ll n;
+    cin >> n;
+
+    vll arr(n);
+    rep(i, 0, n) cin >> arr[i];
+
+    ll i = 0, j = n - 1, sum1 = 0, sum2 = 0, ans = 0;
+    while (i <= j)
+    {
+        if (sum1 <= sum2)
+            sum1 += arr[i++];
+        else
+            sum2 += arr[j--];
+
+        if (sum1 == sum2)
+            ans = sum1;
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast_IO;
+
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    return 0;
+}
