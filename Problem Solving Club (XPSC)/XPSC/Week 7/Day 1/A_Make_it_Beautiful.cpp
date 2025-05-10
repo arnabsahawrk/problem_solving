9d019e34907010bbb4538dfe870585c0ce7985f0
arnabw <arnabsahawrk@gmail.com>
2025-05-10 20:11:03 +0600

    5/10/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 7/Day 1/A_Make_it_Beautiful.cpp
@@ -0,0 +1,73 @@
+/*   Author: Arnab Saha  Date: 05/10/2025 [14:01:10]   */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define ld long double
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define pb push_back
+#define eb emplace_back
+#define sz(x) x.size()
+#define all(v) (v).begin(), (v).end()
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define endl '\n'
+#define out(x) cout << x << endl
+
+const int inf = 1e9;
+const ll linf = 1e18;
+const int mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    ll n;
+    cin >> n;
+
+    map<ll, ll> mp;
+    rep(i, 0, n)
+    {
+        ll x;
+        cin >> x;
+
+        mp[x]++;
+    }
+
+    if (sz(mp) == 1)
+    {
+        out("NO");
+        return;
+    }
+
+    vll arr;
+    each(x, mp)
+        rep(i, 0, x.second) arr.eb(x.first);
+
+    sort(all(arr));
+
+    swap(arr[0], arr[n - 1]);
+    swap(arr[1], arr[n - 1]);
+
+    out("YES");
+    each(x, arr) cout << x << " ";
+    cout << endl;
+}
+
+int main()
+{
+    fast_IO;
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    return 0;
+}
