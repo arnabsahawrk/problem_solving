b135af4ad5e580077da42d5fc2f7d100e6441fa5
arnabw <arnabsahawrk@gmail.com>
2025-05-11 01:49:21 +0600

    5/11/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 7/Day 1/two_power_shortcut.cpp
@@ -0,0 +1,46 @@
+/*   Author: Arnab Saha  Date: 05/11/2025 [01:42:15]   */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define ld long double
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define pb push_back
+#define eb emplace_back
+#define sz(x) x.size()
+#define all(v) (v).begin(), (v).end()
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define endl '\n'
+#define out(x) cout << x << endl
+
+const int inf = 1e9;
+const ll linf = 1e18;
+const int mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    cout << (1 << 4) << endl; // int limit 2^31
+
+    cout << (1LL << 45) << endl; // type convert into LL
+}
+
+int main()
+{
+    fast_IO;
+
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    return 0;
+}
