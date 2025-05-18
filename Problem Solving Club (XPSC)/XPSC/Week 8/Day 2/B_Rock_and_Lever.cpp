fed7702b549b99edab3f40fdf6b5d7c49df6c88d
arnabw <arnabsahawrk@gmail.com>
2025-05-19 03:04:40 +0600

    5/19/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 8/Day 2/B_Rock_and_Lever.cpp
@@ -0,0 +1,65 @@
+/*   Author: Arnab Saha  Date: 05/19/2025 [01:36:51]   */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define ld long double
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define pb push_back
+#define eb emplace_back
+#define sz(x) x.size()
+#define all(v) (v).begin(), (v).end()
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define endl '\n'
+#define out(x) cout << x << endl
+
+const int inf = 1e9;
+const ll linf = 1e18;
+const int mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    ll n;
+    cin >> n;
+
+    map<ll, ll> mp, freq;
+    while (n--)
+    {
+        ll x;
+        cin >> x;
+        mp[__lg(x)]++;
+    }
+    ll cnt = 0;
+    each(x, mp)
+    {
+        if (x.second > 1)
+        {
+            ll n = x.second - 1;
+
+            cnt += ((n * (n + 1)) / 2);
+        }
+    }
+
+    out(cnt);
+}
+
+int main()
+{
+    fast_IO;
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    return 0;
+}
---
+++ Problem Solving Club (XPSC)/XPSC/Week 8/Day 2/C_Increasing_Sequence_with_Fixed_OR.cpp
@@ -0,0 +1,61 @@
+/*   Author: Arnab Saha  Date: 05/18/2025 [21:47:05]   */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define ld long double
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define pb push_back
+#define eb emplace_back
+#define sz(x) x.size()
+#define all(v) (v).begin(), (v).end()
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define endl '\n'
+#define out(x) cout << x << endl
+
+const int inf = 1e9;
+const ll linf = 1e18;
+const int mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    ll n;
+    cin >> n;
+
+    vll ans;
+    ans.eb(n);
+    for (int i = 0; i <= __lg(n); i++)
+    {
+        if ((n >> i) & 1)
+        {
+            if (n - (1LL << i) != 0)
+                ans.eb(n - (1LL << i));
+        }
+    }
+
+    out(sz(ans));
+    rrep(i, sz(ans) - 1, 0) cout << ans[i] << " ";
+
+    cout << endl;
+}
+
+int main()
+{
+    fast_IO;
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    return 0;
+}
