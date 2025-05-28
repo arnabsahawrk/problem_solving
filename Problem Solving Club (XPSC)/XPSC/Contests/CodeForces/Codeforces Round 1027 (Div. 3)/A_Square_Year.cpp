6c063b4324424625b0fbe14c339592e3e501753b
arnabw <arnabsahawrk@gmail.com>
2025-05-28 19:53:07 +0600

    5/28/25

---
+++ Problem Solving Club (XPSC)/XPSC/Contests/CodeForces/Codeforces Round 1027 (Div. 3)/A_Square_Year.cpp
@@ -0,0 +1,64 @@
+/*   Author: Arnab Saha  Date: 05/26/2025 [20:41:01]   */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pbds tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define ld long double
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define pb push_back
+#define eb emplace_back
+#define sz(x) x.size()
+#define all(v) (v).begin(), (v).end()
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define endl '\n'
+#define out(x) cout << x << endl
+
+const int inf = 1e9;
+const ll linf = 1e18;
+const int mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    string s;
+    cin >> s;
+
+    string a, b;
+    a.pb(s[0]);
+    a.pb(s[1]);
+    b.pb(s[2]);
+    b.pb(s[3]);
+
+    ll x = stoi(s);
+    ll sq = round(sqrt(x));
+    ll pw = round(pow(sq, 2));
+
+    if (pw == x)
+        cout << 0 << " " << sq << endl;
+    else
+        out(-1);
+}
+
+int main()
+{
+    fast_IO;
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    return 0;
+}
---
+++ Problem Solving Club (XPSC)/XPSC/Week 9/Day 3/D_Taxes.cpp
@@ -0,0 +1,79 @@
+/*   Author: Arnab Saha  Date: 05/26/2025 [18:29:59]   */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pbds tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define ld long double
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define pb push_back
+#define eb emplace_back
+#define sz(x) x.size()
+#define all(v) (v).begin(), (v).end()
+#define ff first
+#define ss second
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define endl '\n'
+#define out(x) cout << x << endl
+
+const int inf = 1e9;
+const ll linf = 1e18;
+const int mod = 1e9 + 7;
+
+bool isPrime(ll n)
+{
+    if (n < 2)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void arnabsahawrk()
+{
+    ll n;
+    cin >> n;
+
+    if (isPrime(n))
+    {
+        out(1);
+    }
+    else if (n & 1)
+    {
+        ll t = 1;
+
+        n -= 2;
+
+        if (isPrime(n))
+            out(2);
+        else
+            out(3);
+    }
+    else
+        out(2);
+}
+
+int main()
+{
+    fast_IO;
+
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    return 0;
+}
