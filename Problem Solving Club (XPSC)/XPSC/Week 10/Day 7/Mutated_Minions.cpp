1c771fdd1b11be6317af99e783d6c451396893dd
arnabw <arnabsahawrk@gmail.com>
2025-06-08 01:42:57 +0600

    6/8/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 10/Day 7/Mutated_Minions.cpp
@@ -0,0 +1,63 @@
+/*   Author: Arnab Saha  Date: 06/07/2025 [16:14:26]   */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pbds tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define ld long double
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define pb push_back
+#define eb emplace_back
+#define sz(x) x.size()
+#define all(v) (v).begin(), (v).end()
+#define ff first
+#define ss second
+#define rep(i, a, b) for (ll i = a; i < b; ++i)
+#define rrep(i, a, b) for (ll i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define endl '\n'
+#define out(x) cout << x << endl
+
+const int inf = 1e9;
+const ll linf = 1e18;
+const int mod = 1e9 + 7;
+
+int main()
+{
+    fast_IO;
+
+    auto arnabsahawrk = [&]()
+    {
+        ll n, k;
+        cin >> n >> k;
+
+        ll ans = 0;
+        while (n--)
+        {
+            ll x;
+            cin >> x;
+
+            x += k;
+
+            if (x % 7 == 0)
+                ans++;
+        }
+
+        out(ans);
+    };
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    return 0;
+}
---
+++ Problem Solving Club (XPSC)/XPSC/Week 10/Day 7/The_Lead_Game.cpp
@@ -0,0 +1,68 @@
+/*   Author: Arnab Saha  Date: 06/07/2025 [18:51:23]   */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pbds tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define fast_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define ll long long
+#define ld long double
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define pb push_back
+#define eb emplace_back
+#define sz(x) x.size()
+#define all(v) (v).begin(), (v).end()
+#define ff first
+#define ss second
+#define rep(i, a, b) for (ll i = a; i < b; ++i)
+#define rrep(i, a, b) for (ll i = a; i >= b; --i)
+#define each(x, a) for (auto &x : a)
+#define endl '\n'
+#define out(x) cout << x << endl
+
+const int inf = 1e9;
+const ll linf = 1e18;
+const int mod = 1e9 + 7;
+
+int main()
+{
+    fast_IO;
+
+    auto arnabsahawrk = [&]()
+    {
+        ll n;
+        cin >> n;
+
+        ll playerA = 0, playerB = 0, leader = 0, maxLead = 0;
+        while (n--)
+        {
+            ll a, b;
+            cin >> a >> b;
+
+            playerA += a, playerB += b;
+
+            ll currLead = abs(playerA - playerB);
+
+            if (currLead > maxLead)
+            {
+                maxLead = currLead;
+                leader = a > b ? 1 : 2;
+            }
+        }
+
+        cout << leader << " " << maxLead << endl;
+    };
+
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    return 0;
+}
