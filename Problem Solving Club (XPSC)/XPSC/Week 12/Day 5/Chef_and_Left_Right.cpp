f60d13e4084968e0658ac6ff663a66272cf4a62a
arnabw <arnabsahawrk@gmail.com>
2025-06-19 01:58:00 +0600

    6/19/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 12/Day 5/Chef_and_Left_Right.cpp
@@ -0,0 +1,74 @@
+/*   Author: Arnab Saha  Date: 06/18/2025 [18:36:48]   */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pbds tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define checkmate return 0;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using vs = vector<string>;
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
+#define rep(i, a, b) for (ll i = (a); i < (b); ++i)
+#define rrep(i, a, b) for (ll i = (a); i >= (b); --i)
+#define each(x, a) for (auto &x : a)
+#define rin(v) each(i, v) cin >> i
+#define out(x) cout << x << endl
+#define endl '\n'
+
+const ll inf = 1e9;
+const ll linf = 1e18;
+const ll mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    string s;
+    cin >> s;
+
+    ll ans = 1;
+    rep(i, 0, sz(s))
+    {
+        if (i & 1)
+        {
+            if (s[i] == 'l')
+                ans= (ans * 2) - 1;
+            else
+                ans= (ans * 2) + 1;
+        }
+        else
+        {
+            if (s[i] == 'l')
+                ans= (ans * 2);
+            else
+                ans= (ans * 2) + 2;
+        }
+        ans %= mod;
+    }
+
+    out(ans);
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    ll tc = 1;
+    cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    checkmate;
+}
