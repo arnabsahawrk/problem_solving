6edb484105d16d37773bc87f23ccfee75cecd8fc
arnabw <arnabsahawrk@gmail.com>
2025-06-30 01:50:20 +0600

    6/30/25

---
+++ Problem Solving Club (XPSC)/XPSC/Contests/Virtual Contest/Codeforces Round 1009 (Div. 3)/C_XOR_and_Triangle.cpp
@@ -0,0 +1,73 @@
+/*   Author: Arnab Saha  Date: 06/29/2025 [21:07:53]   */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pbds tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define checkmate return 0;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using vs = vector<string>;
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
+#define rep(i, a, b) for (ll i = (a); i < (b); ++i)
+#define rrep(i, a, b) for (ll i = (a); i >= (b); --i)
+#define each(x, a) for (auto &x : a)
+#define rin(v) each(i, v) cin >> i
+#define out(x) cout << x << endl
+#define endl '\n'
+
+const ll inf = 1e9;
+const ll linf = 1e18;
+const ll mod = 1e9 + 7;
+
+ll on_bit(ll n, ll k)
+{
+    return n | (1LL << k);
+}
+
+void arnabsahawrk()
+{
+    ll x;
+    cin >> x;
+
+    ll msb = __lg(x);
+    ll y = 1;
+    rep(i, 0, msb)
+    {
+        y = on_bit(y, i);
+        ll z = x ^ y;
+
+        if (x + y > z && x + z > y && y + z > x)
+        {
+            out(y);
+            return;
+        }
+    }
+
+    out(-1);
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    ll tc = 1;
+    cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    checkmate;
+}
