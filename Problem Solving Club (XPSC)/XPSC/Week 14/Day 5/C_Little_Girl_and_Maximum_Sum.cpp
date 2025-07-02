5f2b06700436130783ecbbdef828c508a8ae451b
arnabw <arnabsahawrk@gmail.com>
2025-07-02 15:27:27 +0600

    7/2/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 14/Day 5/C_Little_Girl_and_Maximum_Sum.cpp
@@ -0,0 +1,80 @@
+/*   Author: Arnab Saha  Date: 07/02/2025 [12:51:44]   */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pbds tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define checkmate return 0;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using vs = vector<string>;
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
+#define rep(i, a, b) for (ll i = (a); i < (b); ++i)
+#define rrep(i, a, b) for (ll i = (a); i >= (b); --i)
+#define each(x, a) for (auto &x : a)
+#define rin(v) each(i, v) cin >> i
+#define out(x) cout << x << endl
+#define endl '\n'
+
+const ll inf = 1e9;
+const ll linf = 1e18;
+const ll mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    ll n, q;
+    cin >> n >> q;
+
+    vll arr(n);
+    rin(arr);
+
+    vll diffArr(n);
+    while (q--)
+    {
+        ll l, r;
+        cin >> l >> r;
+
+        l--;
+        r--;
+
+        diffArr[l]++;
+        if (r != n - 1)
+            diffArr[r + 1]--;
+    }
+
+    rep(i, 1, n) diffArr[i] += diffArr[i - 1];
+
+    sort(arr.rbegin(), arr.rend());
+    sort(diffArr.rbegin(), diffArr.rend());
+
+    ll sum = 0;
+    rep(i, 0, n)
+        sum += (arr[i] * diffArr[i]);
+
+    out(sum);
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    checkmate;
+}
