60f4feb39416408fac9ab1e79072f64b6711dd27
arnabw <arnabsahawrk@gmail.com>
2025-07-05 16:43:24 +0600

    7/5/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 15/Day 1/A_Greg_and_Array.cpp
@@ -0,0 +1,79 @@
+/*   Author: Arnab Saha  Date: 07/05/2025 [14:45:54]   */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pbds tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define checkmate return 0;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using vs = vector<string>;
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
+#define rep(i, a, b) for (ll i = (a); i < (b); ++i)
+#define rrep(i, a, b) for (ll i = (a); i >= (b); --i)
+#define each(x, a) for (auto &x : a)
+#define rin(v) each(i, v) cin >> i
+#define out(x) cout << x << endl
+#define endl '\n'
+
+const ll inf = 1e9;
+const ll linf = 1e18;
+const ll mod = 1e9 + 7;
+
+const ll N = 1e5 + 9;
+ll a[N], l[N], r[N], d[N], q[N], diff[N];
+
+void arnabsahawrk()
+{
+    ll n, m, k;
+    cin >> n >> m >> k;
+
+    rep(i, 1, n + 1) cin >> a[i];
+    rep(i, 1, m + 1) cin >> l[i] >> r[i] >> d[i];
+    rep(i, 1, k + 1)
+    {
+        ll x, y;
+        cin >> x >> y;
+
+        q[x]++;
+        q[y + 1]--;
+    }
+
+    rep(i, 1, m + 1) q[i] += q[i - 1];
+    rep(i, 1, n + 1) diff[i] = a[i] - a[i - 1];
+
+    rep(i, 1, m + 1)
+    {
+        diff[l[i]] += d[i] * q[i];
+        diff[r[i] + 1] -= d[i] * q[i];
+    }
+
+    rep(i, 1, n + 1) a[i] = a[i - 1] + diff[i];
+
+    rep(i, 1, n + 1) cout << a[i] << " ";
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    checkmate;
+}
