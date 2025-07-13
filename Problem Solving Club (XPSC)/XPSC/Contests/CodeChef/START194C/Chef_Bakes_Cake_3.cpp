2c092316d3daf7dcc3efd5eb299e282a51891e8d
arnabw <arnabsahawrk@gmail.com>
2025-07-13 23:59:04 +0600

    7/13/25

---
+++ Problem Solving Club (XPSC)/XPSC/Contests/CodeChef/START194C/Chef_Bakes_Cake_3.cpp
@@ -0,0 +1,98 @@
+/*   Author: Arnab Saha  Date: 07/13/2025 [20:49:59]   */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pbds tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define checkmate return 0;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using vs = vector<string>;
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
+#define rep(i, a, b) for (ll i = (a); i < (b); ++i)
+#define rrep(i, a, b) for (ll i = (a); i >= (b); --i)
+#define each(x, a) for (auto &x : a)
+#define rin(v) each(i, v) cin >> i
+#define out(x) cout << x << endl
+#define endl '\n'
+
+const ll inf = 1e9;
+const ll linf = 1e18;
+const ll mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    ll n;
+    cin >> n;
+
+    vll arr(n);
+    rin(arr);
+
+    /* sort(all(arr));
+
+    ll m = n / 2;
+    if (n % 2 == 0)
+        m--;
+    m = arr[m];
+
+    ll cost = (m * n) * 30;
+    ll profit = 0;
+    rep(i, 0, n)
+    {
+        if (arr[i] < m)
+            profit += arr[i];
+        else
+            profit += m;
+    }
+
+    profit *= 50;
+    profit -= cost;
+
+    out(profit); */
+
+    ll profit = 0;
+
+    rep(i, 0, n)
+    {
+        ll x = arr[i], sell = 0;
+        rep(j, 0, n)
+        {
+            if (arr[j] < x)
+                sell += arr[j];
+            else
+                sell += x;
+        }
+
+        ll cost = (x * n) * 30;
+        sell *= 50;
+
+        profit = max(profit, (sell - cost));
+    }
+
+    out(profit);
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    ll tc = 1;
+    cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    checkmate;
+}
