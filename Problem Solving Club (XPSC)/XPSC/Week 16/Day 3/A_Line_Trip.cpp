083f7ecd5a4249bb6d2ffec9f54131d0e6eb7f03
arnabw <arnabsahawrk@gmail.com>
2025-07-14 05:07:52 +0600

    7/14/25

---
+++ Problem Solving Club (XPSC)/XPSC/Week 16/Day 3/A_Line_Trip.cpp
@@ -0,0 +1,63 @@
+/*   Author: Arnab Saha  Date: 07/14/2025 [04:53:33]   */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pbds tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define checkmate return 0;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using vs = vector<string>;
+
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
+#define rep(i, a, b) for (ll i = (a); i < (b); ++i)
+#define rrep(i, a, b) for (ll i = (a); i >= (b); --i)
+#define each(x, a) for (auto &x : a)
+#define rin(v) each(i, v) cin >> i
+#define out(x) cout << x << endl
+#define endl '\n'
+
+const ll inf = 1e9;
+const ll linf = 1e18;
+const ll mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    ll n, x;
+    cin >> n >> x;
+
+    vll arr(n);
+    rin(arr);
+
+    ll ans = arr.front();
+    rep(i, 1, n)
+        ans = max(ans, arr[i] - arr[i - 1]);
+
+    ans = max(ans, (x - arr.back()) * 2);
+
+    out(ans);
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    ll tc = 1;
+    cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    checkmate;
+}
