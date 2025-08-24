9fed52f7a98ff212c5009dc1d8c18c68e423b57d
arnabw <arnabsahawrk@gmail.com>
2025-08-24 22:29:12 +0600

    8/24/25

---
+++ Problem Solving Club (XPSC)/XPSC/Contests/CodeForces/Codeforces Round 1044 (Div. 2)/B_Villagers.cpp
@@ -0,0 +1,49 @@
+/*   Author: Arnab Saha  Date: 08/24/2025 [21:19:14]   */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define pbds tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define checkmate return 0;
+
+using ll = long long;
+using ld = long double;
+
+#define out(x) cout << x << endl
+#define endl '\n'
+
+const int mod = 1e9 + 7;
+
+void arnabsahawrk()
+{
+    int n;
+    cin >> n;
+
+    vector<ll> g(n);
+    for (int i = 0; i < n; i++)
+        cin >> g[i];
+
+    sort(g.begin(), g.end());
+
+    ll ans = 0;
+    for (int i = n - 1; i >= 0; i -= 2)
+        ans += g[i];
+
+    out(ans);
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        arnabsahawrk();
+
+    checkmate;
+}
